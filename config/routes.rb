461c12f307feed831bb9d1739b5d9969ced64cc7
BenMiriello <benmiriello@gmail.com>
2020-01-15 18:00:25 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show'
+  get '/sightings' => 'sightings#index'
+  get '/sightings/:id' => 'sightings#show'
 end
